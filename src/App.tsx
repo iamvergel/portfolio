05ea3546c3ce6108c0ffbfbf67958f28460b7feb
Vergel Macayan <94885341+iamvergel@users.noreply.github.com>
2025-06-10 20:00:53 +0800

    aa

--- src/App.tsx
+++ src/App.tsx
@@ -10,17 +10,30 @@ import About from "./main/about";
 import Projects from "./main/projects";
 
 export default function App() {
+  // const images = [
+  //   "/portfolio/public/assets/images/bg-image.png",
+  //   "/portfolio/public/assets/images/bg-image1.png",
+  //   "/portfolio/public/assets/images/bg-image5.png",
+  //   "/portfolio/public/assets/images/bg-image2.png",
+  //   "/portfolio/public/assets/images/bg-image.png",
+  //   "/portfolio/public/assets/images/bg-image6.png",
+  //   "/portfolio/public/assets/images/bg-image3.png",
+  //   "/portfolio/public/assets/images/bg-image4.png",
+  //   "/portfolio/public/assets/images/bg-image7.png",
+  // ];
+
   const images = [
-    "/portfolio/public/assets/images/bg-image.png",
-    "/portfolio/public/assets/images/bg-image1.png",
-    "/portfolio/public/assets/images/bg-image5.png",
-    "/portfolio/public/assets/images/bg-image2.png",
-    "/portfolio/public/assets/images/bg-image.png",
-    "/portfolio/public/assets/images/bg-image6.png",
-    "/portfolio/public/assets/images/bg-image3.png",
-    "/portfolio/public/assets/images/bg-image4.png",
-    "/portfolio/public/assets/images/bg-image7.png",
+    "/assets/images/bg-image.png",
+    "/assets/images/bg-image1.png",
+    "/assets/images/bg-image5.png",
+    "/assets/images/bg-image2.png",
+    "/assets/images/bg-image.png",
+    "/assets/images/bg-image6.png",
+    "/assets/images/bg-image3.png",
+    "/assets/images/bg-image4.png",
+    "/assets/images/bg-image7.png",
   ];
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [prevIndex, setPrevIndex] = useState<number | null>(null);
 
